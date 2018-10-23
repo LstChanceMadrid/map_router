6017252d2e251774b97f57692ae9b861d6f95745
LstChanceMadrid <michael.madrid@live.com>
2018-10-23 17:44:34 -0500

    app.js formatting

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,30 @@
+let mapContainer = document.getElementById('map');
+let navContainer = document.getElementById("nav-container");
+let navButton = document.getElementById("nav-button");
+let loginButton = document.getElementById('login-button')
 
+window.addEventListener("load", function() {
+	
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		mapContainer.style.marginTop = "100px";
+		mapContainer.style.width = "100%"
+		navContainer.style.display = "none"
+		console.log("Screen width less than 768px");
+	} else {
+		navButton.style.display = "none"
+		console.log("Screen more than 768px");
+	}
+});
   
-  window.addEventListener("load", function() {
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      document.getElementById('map').style.marginTop = "100px";
-      document.getElementById('map').style.width = "100%"
-      document.getElementById('nav-container').style.display = "none"
+navButton.addEventListener('click', function() {
+	if (navContainer.style.display === "none") {
+		navContainer.style.display = "flex"
+	} else {
+		navContainer.style.display = "none"
+	}
+});
   
-        console.log("Screen width less than 768px");
-    } else {
-      document.getElementById('nav-button').style.display = "none"
-        console.log("Screen more than 768px");
-    }
-  });
-  
-  document.getElementById("nav-button").addEventListener('click', function() {
-    if (document.getElementById("nav-container").style.display === "none") {
-    document.getElementById("nav-container").style.display = "flex"
-    } else {
-      document.getElementById("nav-container").style.display = "none"
-    }
-  })
-  
-  document.getElementById('login-button').addEventListener('click', function() {
-    document.getElementById("nav-container").style.display = "none"
-  })
+loginButton.addEventListener('click', function() {
+	navContainer.style.display = "none"
+});
   
